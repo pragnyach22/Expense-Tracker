01e559e13002c48dcaa79235c6dc9d40b6b683ce
pragnyach22 <pragnyach22@gmail.com>
2026-06-05 18:04:47 +0530

    Update API URL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({
+  baseURL: 'https://expense-tracker-api-gld0.onrender.com'
+});
 
 // Auth
 export const register  = (d) => API.post('/register', d);
